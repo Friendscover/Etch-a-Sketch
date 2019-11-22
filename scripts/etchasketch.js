8c640dcf1831775c04198d9111b170a5fb290f1b
Tobias Klöpper <tobiaskloepper1@web.de>
2019-11-22 10:28:21 +0100

    Mouseover changes color, pushed for remote work

--- scripts/etchasketch.js
+++ scripts/etchasketch.js
@@ -4,15 +4,31 @@ function createDivs(divs)
     const container = document.querySelector('.container');
     const square = document.createElement('div');
     square.classList.add("square");
-    square.textContent = "Hello!";
+    square.setAttribute('style', 'width: 100px; height: 100px')
+    square.style.height = divs + "px";
+    square.style.width = divs +"px";
 
     for (i= 0; i < divs; i++)
     {  
         for(j = 0; j < divs; j++)
         {
             container.appendChild(square.cloneNode());
+            
         }
-
-       // container.appendChild(square.cloneNode());
     }
+    addColorChanger();
 }
+
+function addColorChanger()
+{
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "grey";
+        });
+    });
+}
+
+
+createDivs(10);
+
