607d2a049acdedb236c508e155205aea3ed3fcbe
Tobias Klöpper <tobiaskloepper1@web.de>
2019-11-26 20:59:16 +0100

    Basic Functionality finished, rework of div Creation Needed

--- scripts/etchasketch.js
+++ scripts/etchasketch.js
@@ -1,22 +1,31 @@
 
-function createDivs(divs) 
-{
-    const container = document.querySelector('.container');
-    const square = document.createElement('div');
-    square.classList.add("square");
-    square.setAttribute('style', 'width: 100px; height: 100px')
-    square.style.height = divs + "px";
-    square.style.width = divs +"px";
+function createGrid() 
+{   
+    let gridSize = parseInt(prompt("Input a Grid Size betweeen 1 and 64!"));
+    if (gridSize >= 1 && gridSize <= 64)
+    {
+        const container = document.querySelector('.container');    
+        const square = document.createElement('div');
+        square.classList.add("square");
+        //creating the Grid by calculating the Divs Height and Width from the User's Requested Amount of Divs
+        let sizeOfDivs = (960 / gridSize);
+        square.style.height = sizeOfDivs + "px";
+        square.style.width = sizeOfDivs +"px";
 
-    for (i= 0; i < divs; i++)
-    {  
-        for(j = 0; j < divs; j++)
-        {
-            container.appendChild(square.cloneNode());
-            
+        for (i= 0; i < gridSize; i++)
+        {  
+            for(j = 0; j < gridSize; j++)
+            {
+                container.appendChild(square.cloneNode());
+            }
+
         }
+        addColorChanger();
+    } 
+    else 
+    {
+        alert("That number is out of range, try again!");
     }
-    addColorChanger();
 }
 
 function addColorChanger()
@@ -29,6 +38,22 @@ function addColorChanger()
     });
 }
 
+function resetGrid()
+{
+    const container = document.querySelector('.container');
+    while (container.firstChild) 
+    {
+        container.removeChild(container.firstChild);
+    }
+   
+}
 
-createDivs(10);
+const buttonreset = document.querySelector("#btnreset");
+buttonreset.addEventListener('click', () => {
+    resetGrid();
+});
 
+const buttoncreate = document.querySelector("#btncreate");
+buttoncreate.addEventListener('click', () => {
+    createGrid();
+});
